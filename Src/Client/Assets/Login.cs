8b8ee8fd4c9eba3a97be8ca947c7e277f413b915
qianhang <563784623@qq.com>
2022-06-08 22:41:00 +0800

    基础场景与UI作业

---
+++ Src/Client/Assets/Login.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Network.NetClient.Instance.Init("127.0.0.1", 8000);
+        Network.NetClient.Instance.Connect();
+
+        SkillBridge.Message.NetMessage msg = new SkillBridge.Message.NetMessage();
+        msg.Request = new SkillBridge.Message.NetMessageRequest();
+
+        SkillBridge.Message.FirstTestRequest a = new SkillBridge.Message.FirstTestRequest();
+        a.myKey = "myKey";
+
+        msg.Request.firstRequest = a;
+
+        Network.NetClient.Instance.SendMessage(msg);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
